3a805efad3794bbef02bfadf5e16ffc180085b91
natehallpc <natehallpc@gmail.com>
2022-11-18 11:18:06 -0700

    Epsilon Greedy Q-Learning implemented

--- reinforcement/qlearningAgents.py
+++ reinforcement/qlearningAgents.py
@@ -111,10 +111,12 @@ class QLearningAgent(ReinforcementAgent):
         # Pick Action
         legalActions = self.getLegalActions(state)
         action = None
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        
+        if util.flipCoin(self.epsilon):
+          return random.choice(legalActions)
+        else:
+          return self.computeActionFromQValues(state)
 
-        return action
 
     def update(self, state, action, nextState, reward):
         """
